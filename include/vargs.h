7af2d703b58003eb04a57b5af264b78c8340ad1d
nekopara-orz <1070168724@qq.com>
2020-06-10 02:14:36 -0700

    code printk.c and debug.c were  updated

---
+++ include/vargs.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  vargs.h
+ *
+ *    Description:  可变形参表相关的宏定义
+ *
+ *        Version:  1.0
+ *        Created:  2020年06月10日
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Pickle
+ *
+ * =====================================================================================
+ */
+
+#ifndef INCLUDE_VARGS_H_
+#define INCLUDE_VARGS_H_
+
+typedef __builtin_va_list va_list;
+
+#define va_start(ap, last)         (__builtin_va_start(ap, last))
+#define va_arg(ap, type)           (__builtin_va_arg(ap, type))
+#define va_end(ap) 
+
+#endif 	// INCLUDE_VARGS_H_
