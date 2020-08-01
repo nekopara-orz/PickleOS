28e51f61772c135641c1bd8b3dc97219bfdc5515
nekopara-orz <1070168724@qq.com>
2020-08-01 03:08:13 -0700

    add idt

--- init/entry.c
+++ init/entry.c
@@ -58,17 +58,20 @@ int kern_entry()
 #include "string.h"
 #include "debug.h"
 #include "gdt.h"
+#include "idt.h"
 int kern_entry()
 {
 	
 	init_debug();
 	init_gdt();
+	init_idt();
 
 	console_clear();
 	printk_color(rc_black, rc_green, "Hello, OS my_kernel!\n");
-	panic("test");
+	//panic("test");
 	
-	
+	asm volatile ("int $0x3");
+	asm volatile ("int $0x4");
 
 	return 0;
 }
