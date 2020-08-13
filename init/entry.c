09fae11db1d47fea9b43e6b8de428c1867730924
root <1070168724@qq.com>
2020-08-13 01:54:00 -0700

    add page_management/pmm

--- init/entry.c
+++ init/entry.c
@@ -59,6 +59,8 @@ int kern_entry()
 #include "debug.h"
 #include "gdt.h"
 #include "idt.h"
+#include "timer.h"
+#include "pmm.h"
 int kern_entry()
 {
 	
@@ -75,6 +77,25 @@ int kern_entry()
 	init_timer(200);
 
 	// 开启中断
-	asm volatile ("sti");
+	//asm volatile ("sti");
+	printk("kernel in memory start: 0x%08X\n", kern_start);
+	printk("kernel in memory end:   0x%08X\n", kern_end);
+	printk("kernel in memory used:   %d KB\n\n", (kern_end - kern_start) / 1024);
+	show_memory_map();
+	init_pmm();
+
+	printk_color(rc_black, rc_red, "\nThe Count of Physical Memory Page is: %u\n\n", phy_page_count);
+	
+	uint32_t allc_addr = NULL;
+	printk_color(rc_black, rc_light_brown, "Test Physical Memory Alloc :\n");
+	allc_addr = pmm_alloc_page();
+	printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+	allc_addr = pmm_alloc_page();
+	printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+	allc_addr = pmm_alloc_page();
+	printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+	allc_addr = pmm_alloc_page();
+	printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+
 	return 0;
 }
